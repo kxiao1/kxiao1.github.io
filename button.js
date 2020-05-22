00a5934f2ef0fdc0a90402d8785ddebc1d7a6fac
Karl Xiao <xzetianx1997@gmail.com>
2020-05-22 14:39:59 +0000

    style fixes

--- button.js
+++ button.js
@@ -1,17 +1,17 @@
-const links = document.querySelectorAll(".btn:not(.teaser)");
+const links = document.querySelectorAll('.btn:not(.teaser)');
 var oColor = new Array(links.length);
-var oText = oColor 
+var oText = oColor
 for (let i = 0; i < links.length; i++) {
   oColor[i] = links[i].style.backgroundColor;
   oText[i] = links[i].style.color
 }
 for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("mouseover",function(e) {
-    e.target.style.backgroundColor = "#000000";
-    e.target.style.color = "#f3f3f4";
-  },false);
-  links[i].addEventListener("mouseout",function(e) {
+  links[i].addEventListener('mouseover', function(e) {
+    e.target.style.backgroundColor = '#000000';
+    e.target.style.color = '#f3f3f4';
+  }, false);
+  links[i].addEventListener('mouseout', function(e) {
     e.target.style.backgroundColor = oColor[i];
     e.target.style.color = oText[i];
-  },false);
+  }, false);
 }
--- writeup.js
+++ writeup.js
@@ -1,31 +1,30 @@
-const travels = document.querySelectorAll(".btn-outline-dark");
+const travels = document.querySelectorAll('.btn-outline-dark');
 var len = travels.length;
 let txt = new Array(len);
-txt[0] = String.fromCharCode(8226) + 
-          " Solo trip to Panama (Boquete is highly recommended!)\n" + 
-          String.fromCharCode(8226) +
-          " Habitat for Humanity trip to West Palm Beach, FL\n" +
-          String.fromCharCode(8658);
-txt[1] = String.fromCharCode(8226) + 
-          " Family trip to the lovely city of Penang! \n" + 
-          String.fromCharCode(8226) +
-          " Too much sumptuous food to post here so check this out...\n" +
-          String.fromCharCode(8658);
-txt[2] = String.fromCharCode(8226) + 
-          " Solo trip to Britain (London, Fountains Abbey, " +
-          "Cambridge, Edinburgh) " +
-          "and Denmark (Copenhagen, Helsingør).\n" +
-          String.fromCharCode(8226) + 
-          " Check out the Harry Potter Studios just outside London!\n" +
-          String.fromCharCode(8658);
+txt[0] = String.fromCharCode(8226) +
+    ' Solo trip to Panama (Boquete is highly recommended!)\n' +
+    String.fromCharCode(8226) +
+    ' Habitat for Humanity trip to West Palm Beach, FL\n' +
+    String.fromCharCode(8658);
+txt[1] = String.fromCharCode(8226) +
+    ' Family trip to the lovely city of Penang! \n' +
+    String.fromCharCode(8226) +
+    ' Too much sumptuous food to post here so check this out...\n' +
+    String.fromCharCode(8658);
+txt[2] = String.fromCharCode(8226) +
+    ' Solo trip to Britain (London, Fountains Abbey, ' +
+    'Cambridge, Edinburgh) ' +
+    'and Denmark (Copenhagen, Helsingør).\n' + String.fromCharCode(8226) +
+    ' Check out the Harry Potter Studios just outside London!\n' +
+    String.fromCharCode(8658);
 let link = new Array(len);
-link[0] = "https://www.boqueteoutdooradventures.com/";
-link[1] = "https://girleatworld.net/penang-food-guide/";
-link[2] = "https://www.wbstudiotour.co.uk/";
+link[0] = 'https://www.boqueteoutdooradventures.com/';
+link[1] = 'https://girleatworld.net/penang-food-guide/';
+link[2] = 'https://www.wbstudiotour.co.uk/';
 let desc = new Array(len);
-desc[0] = "Boquete Outdoor Adventures";
-desc[1] = "Penang Food Guide"
-desc[2] = "Warner Bros Studio Tour"
+desc[0] = 'Boquete Outdoor Adventures';
+desc[1] = 'Penang Food Guide'
+desc[2] = 'Warner Bros Studio Tour'
 let clicked = new Array(travels.length);
 clicked[0] = false;
 clicked[1] = false;
@@ -38,50 +37,54 @@ function showText(i) {
   var t = document.createTextNode(txt[i]);
   var a = document.createElement('a');
   a.href = link[i];
-  a.target = "_blank";
+  a.target = '_blank';
   a.innerHTML = desc[i];
   myP.appendChild(t);
   myP.appendChild(a);
-  myP.style = "white-space:pre-line";
-  myP.style.width = "100%";
-  myP.style.fontSize = "120%";
-  myP.style.fontSize = "1.3vw";
+  myP.style = 'white-space:pre-line';
+  myP.style.width = '100%';
+  myP.style.fontSize = '120%';
+  myP.style.fontSize = '1.3vw';
   // myP.style.height = "10%";
-  myP.style.border = "1.5px solid #888888";
-  myP.style.borderRadius = "2%";
-  myP.style.padding = "0.375rem 0.75rem";
-  var newdiv = document.createElement("div");
-  newdiv.id="newdiv" + i;
-  newdiv.style.width = "100%";
-  newdiv.style.padding = "15px";
+  myP.style.border = '1.5px solid #888888';
+  myP.style.borderRadius = '2%';
+  myP.style.padding = '0.375rem 0.75rem';
+  var newdiv = document.createElement('div');
+  newdiv.id = 'newdiv' + i;
+  newdiv.style.width = '100%';
+  newdiv.style.padding = '15px';
   newdiv.appendChild(myP);
-  var mydiv = document.getElementById("desc");
+  var mydiv = document.getElementById('desc');
   for (var k = 0; k < len; k++) {
     console.log(clicked[i]);
     if (clicked[k]) {
       hideText(k);
     }
   }
-  mydiv.insertBefore(newdiv,mydiv.childNodes[1]);
-  travels[i].removeEventListener("click", show[i]);
+  mydiv.insertBefore(newdiv, mydiv.childNodes[1]);
+  travels[i].removeEventListener('click', show[i]);
   clicked[i] = true;
-  travels[i].addEventListener("click",hide[i],false);
+  travels[i].addEventListener('click', hide[i], false);
 }
 
 function hideText(i) {
-  var mydiv = document.getElementById("desc");
-  var newdiv = document.getElementById("newdiv" + i);
+  var mydiv = document.getElementById('desc');
+  var newdiv = document.getElementById('newdiv' + i);
   mydiv.removeChild(newdiv);
   clicked[i] = false;
-  travels[i].addEventListener("click",show[i],false);
-  travels[i].removeEventListener("click",hide[i],false);
+  travels[i].addEventListener('click', show[i], false);
+  travels[i].removeEventListener('click', hide[i], false);
 }
 
 for (let i = 0; i < len; i++) {
-  show[i] = function() {showText(i)};
-  hide[i] = function() {hideText(i)}
+  show[i] = function() {
+    showText(i)
+  };
+  hide[i] = function() {
+    hideText(i)
+  }
 }
 
 for (let i = 0; i < len; i++) {
-  travels[i].addEventListener("click",show[i],false);
+  travels[i].addEventListener('click', show[i], false);
 }
